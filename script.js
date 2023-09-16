db5a9e9cd8736c353d985ca8a80f83595de1adaf
viannieab <123542222+viannieab@users.noreply.github.com>
2023-09-16 14:08:22 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,69 @@
+/*===========toggle icon navbar ===========*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+}
+/*===========scroll sections active link ===========*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        if(top >= offset && top < offset + height) {
+          navLinks.forEach(links => {
+            links.classList.remove('active');
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+          });  
+        };
+    });
+/*====swipper====*/
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 50,
+  loop: true,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+/*===========sticky navbar ===========*/
+let header = document.querySelector('header');
+header.classList.toggle('sticky', window.scrollY > 100);
+
+/*===========remove toggle icon and navbar when click navbar link (scroll)===========*/
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
+};
+
+/*===========scroll reveal ===========*/
+ScrollReveal({ 
+  reset: true,
+  distance: '80px',
+  duration: 2000,
+  delay: 200
+});
+ScrollReveal().reveal('.home-content, .heading', { origin: 'bottom' });
+ScrollReveal().reveal('.home-img, .services-container, .portifolio-box, .contact form', { origin: 'top' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'right' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'left' });
+
+/*===========typed js ===========*/
+const typed = new Typed('.multiple-text', {
+  strings: ['Computer Scientist', 'Data Scientist', 'Consultant'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
